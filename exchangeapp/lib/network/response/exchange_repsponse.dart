2a3a3c35ba643f4cbc194c06f5336958b5dd5fde
Alex Contra <contraalex8@gmail.com>
2022-04-28 20:47:49 +0300

    integrated dependencies

---
+++ exchangeapp/lib/network/response/exchange_repsponse.dart
@@ -0,0 +1,13 @@
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'exchange_repsponse.freezed.dart';
+part 'exchange_repsponse.g.dart';
+
+@freezed
+class ExchangeRepsponse with _$ExchangeRepsponse {
+  factory ExchangeRepsponse() = _ExchangeRepsponse;
+	
+  factory ExchangeRepsponse.fromJson(Map<String, dynamic> json) =>
+			_$ExchangeRepsponseFromJson(json);
+}
